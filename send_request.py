84d3fda474e01b57488ea4ffc7122b8bae4da3b4
kamiladiana <kamilawinnicka@gmail.com>
2025-07-09 20:37:52 +0200

    find_devices

--- send_request.py
+++ send_request.py
@@ -16,14 +16,26 @@ def play_song(access_token, track_uri):
         headers=headers,
         json=data
     )
-
     if response.status_code == 204:
         print("Success")
     else:
         print(f"Error: {response.status_code} - {response.text}")
 
 
+def find_devices(access_token):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+    response = requests.get("https://api.spotify.com/v1/me/player/devices", headers=headers)
+    if response.status_code == 200:
+        devices = response.json().get("devices")
+        print(devices)
+    else:
+        print("Couldn't find devices")
+
+
 if __name__ == '__main__':
-    access_token = "YOUR_ACCESS_TOKEN"
+    access_token = "???"
     track_uri = "spotify:track:0iHZAp0Vxgvr0NjexVWkxy"
     play_song(access_token, track_uri)
